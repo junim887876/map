cb2c4e1715d44edc21106ca00985237691333a53
junim887876 <junimbarberofc@gmail.com>
2026-09-17 12:19:19 -0300

    Add assets to cache in service worker

---
+++ sw.js
@@ -0,0 +1,14 @@
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './casa.glb',
+  './logo_nova.jpg',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js',
+  'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/js/loaders/GLTFLoader.js',
+  'https://cdn.jsdelivr.net/npm/three@0.128.0/examples/js/controls/OrbitControls.js'
+];
